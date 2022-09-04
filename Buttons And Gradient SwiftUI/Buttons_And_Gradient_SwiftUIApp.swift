309479fc3c9db8952305abad73656e02c132ee95
Abdelrhman Rafat <eng.abdelrhman95@gmail.com>
2022-09-04 14:27:34 +0200

    Initial Commit

---
+++ Buttons And Gradient SwiftUI/Buttons_And_Gradient_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Buttons_And_Gradient_SwiftUIApp.swift
+//  Buttons And Gradient SwiftUI
+//
+//  Created by Macbook on 04/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Buttons_And_Gradient_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
